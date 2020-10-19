1bd2a87ca6414fd5fca769acb72058b11441184a
kim Sanghyeon <llmm030506@naver.com>
2020-10-19 11:18:47 +0800

    commit
    commit

--- Chap04/http.js
+++ Chap04/http.js
@@ -32,17 +32,45 @@ var http = require('http');
 
 //#region 3. request, response 메시지
 
-var server = http.createServer(function(req, res){
-    console.log('req.headers ', req.headers);
-    console.log('req.method ', req.method);
-    console.log('req.url ', req.url);
-    
+// var server = http.createServer(function(req, res){
+//     console.log('req.headers ', req.headers);
+//     console.log('req.method ', req.method);
+//     console.log('req.url ', req.url);
+
+//     res.statusCode = 200;
+//     res.statusMessage = 'OKOK';
+//     res.setHeader('Content-Type', 'text/html;charset=utf-8');
+//     res.writeHead(200, 'Okay',{'Content-Type':'text/html;charset=utf-8',
+//     'myName':'kimsanghyeon'});
 
 
 
 
+//     res.end('<h1> 안녕하세요 반갑습니다. </h1>');
+// }).listen(8080, ()=> console.log("8080 포트로 실행함"));
 
-    res.end('<h1> welcom to my server </h1>');
+//#endregion
+
+//#region 
+
+var server = http.createServer(function(req, res){ 
+    res.end(`<!DOCTYPE html>
+    <html>
+      <head>
+        <title>제목</title>
+        <meta charset="utf-8" />
+      </head>
+      <body>
+        <h1>Hello</h1>
+        <div>
+          <p>Welcome to HTML world! 저와 함께 html을 배워봅시다.</p>
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRfxSBeI1_dVUmNt6DjCb0LOmEB-W_aZkmLQg&usqp=CAU"
+          />
+        </div>
+      </body>
+    </html>
+    `);
 }).listen(8080, ()=> console.log("8080 포트로 실행함"));
 
 //#endregion
