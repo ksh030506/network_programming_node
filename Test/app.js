89a2e0ca0926eefd146afc729ab168542a8007ce
ksh030506 <llmm030506@naver.com>
2020-11-11 19:22:25 +0800

    [ Add ]
    수행평가

--- Test/app.js
+++ Test/app.js
@@ -1,24 +1,60 @@
 const express = require('express');
-const fs = require('fs');
-const { request } = require('http');
 const app = express();
-const PORT = 8080;
+const fs = require('fs');
 
-app.get('/', (req,res) => {
-    fs.readFile('./index.html', (err, data) => {
-        if(err){
-            console.log(err);
-            return;
+
+app.get('/', function(req, res){
+    var num1 = parseInt(req.query.num1);
+    var num2 = parseInt(req.query.num2);
+    var operator = req.query.operator;
+
+    if(num1 && num2 && operator){
+        if(operator == 'add'){
+            res.send(`
+            <h1>GET 요청 처리 결과: ${calculate(num1, num2, operator)}</h1>
+            <h1><a href="/">Home</a></h1>
+        `);
+        } else if(operator == 'subtract'){
+            res.send(`
+            <h1>GET 요청 처리 결과: ${calculate(num1, num2, operator)}</h1>
+            <h1><a href="/">Home</a></h1>
+        `);
+        } else if(operator == 'multiply'){
+            res.send(`
+            <h1>GET 요청 처리 결과: ${calculate(num1, num2, operator)}</h1>
+            <h1><a href="/">Home</a></h1>
+        `);
+        } else if(operator == 'divide') {
+            res.send(`
+            <h1>GET 요청 처리 결과: ${calculate(num1, num2, operator)}</h1>
+            <h1><a href="/">Home</a></h1>
+        `);
         }
-        else {
-            res.end(data);
+    } else {
+        fs.readFile('./index.html', function(err, data){
+            if(err){
+                console.log(err);
+            } else {
+                res.end(data);
+            }
+        });
+    }
+
+    function calculate(num1, num2, operator){
+        var sum = 0;
+        if(operator == 'add') {
+            sum = num1 + num2;
+        } else if(operator == 'subtract') {
+            sum = num1 - num2;
+        } else if(operator == 'multiply') {
+            sum = num1 * num2;
+        } else if(operator == 'divide') {
+            sum = num1 / num2;
         }
-    });
-    if(req.query.operator == 'add') {
-        console.log('덧셈');
+        return sum;
     }
 });
 
-app.listen(PORT, () => {
-  console.log(`App is listening on port ${PORT}!`);
+app.listen(8080, () => {
+  console.log(`App is listening on port ${8000}!`);
 });
