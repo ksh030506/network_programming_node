4ed120bce4787d750d4ab58396d5146e12f88363
ksh030506 <llmm030506@naver.com>
2020-11-13 13:35:23 +0800

    Update app.js

--- Test/app.js
+++ Test/app.js
@@ -1,60 +1,48 @@
-const express = require('express');
-const app = express();
-const fs = require('fs');
+var http = require('http');
+var fs = require('fs');
+var querystring = require('querystring');
+var url = require('url');
 
+var server = http.createServer(function(req, res){
 
-app.get('/', function(req, res){
-    var num1 = parseInt(req.query.num1);
-    var num2 = parseInt(req.query.num2);
-    var operator = req.query.operator;
+        var parsedUrl = url.parse(req.url);
+        var parsedQuery = querystring.parse(parsedUrl.query, '&', '=');
 
-    if(num1 && num2 && operator){
-        if(operator == 'add'){
-            res.send(`
-            <h1>GET 요청 처리 결과: ${calculate(num1, num2, operator)}</h1>
-            <h1><a href="/">Home</a></h1>
-        `);
-        } else if(operator == 'subtract'){
-            res.send(`
-            <h1>GET 요청 처리 결과: ${calculate(num1, num2, operator)}</h1>
-            <h1><a href="/">Home</a></h1>
-        `);
-        } else if(operator == 'multiply'){
-            res.send(`
-            <h1>GET 요청 처리 결과: ${calculate(num1, num2, operator)}</h1>
-            <h1><a href="/">Home</a></h1>
-        `);
-        } else if(operator == 'divide') {
-            res.send(`
-            <h1>GET 요청 처리 결과: ${calculate(num1, num2, operator)}</h1>
-            <h1><a href="/">Home</a></h1>
-        `);
-        }
+        let num1 = parseInt(parsedQuery.num1);
+        let num2 = parseInt(parsedQuery.num2);
+        let operator = parsedQuery.operator;
+
+        if(num1 && num2 && operator) {
+        res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
+        res.end(`
+        <h1>계산 결과는 ${calculate(num1, num2, operator)}</h1>
+        <a href='/'>Home</a>`);
     } else {
-        fs.readFile('./index.html', function(err, data){
-            if(err){
-                console.log(err);
-            } else {
+        if(req.url === '/'){
+            fs.readFile('./index.html', (err, data) => {
                 res.end(data);
-            }
-        });
-    }
-
-    function calculate(num1, num2, operator){
-        var sum = 0;
-        if(operator == 'add') {
-            sum = num1 + num2;
-        } else if(operator == 'subtract') {
-            sum = num1 - num2;
-        } else if(operator == 'multiply') {
-            sum = num1 * num2;
-        } else if(operator == 'divide') {
-            sum = num1 / num2;
+            });
         }
-        return sum;
     }
+}).listen(8080, function(){
+    console.log('8080번 포트에서 대기중');
 });
 
-app.listen(8080, () => {
-  console.log(`App is listening on port ${8000}!`);
-});
+function calculate(num1, num2, operator){
+    let sum = 0;
+
+    if(operator == 'add'){
+        sum = num1 + num2;
+    }
+    else if(operator == 'subtract') {
+        sum = num1 - num2;
+    } 
+    else if(operator == 'multiply') {
+        sum = num1 * num2;
+    }
+    else if(operator == 'divide') {
+        sum = num1 / num2;
+    }
+
+    return sum;
+}
